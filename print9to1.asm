ec6e7d71f1a512a5a1b29d2fac037a76f3656889
arafat-rahman01 <aabrarbin851@gmail.com>
2026-08-03 00:17:05 +0600

    print 9 to 1

--- print9to1.asm
+++ print9to1.asm
@@ -1,23 +1,22 @@
 .model small
 .stack 100h
 .code
-
 main proc
-
-    mov cl,9        ; শুরু 9 থেকে
-
-again:
+    mov bl,9
+    
+    again:
+    
     mov ah,2
-    mov dl,cl
-    add dl,30h      ; ASCII তে রূপান্তর
+    mov dl,bl
+    add dl,30h
     int 21h
-
-    dec cl          ; CL = CL - 1
-    cmp cl,0        ; 0 হয়েছে?
-    jne again       ; না হলে আবার Loop
-
-    mov ah,4Ch
+    
+    dec bl
+    cmp bl,1
+    jne again
+    
+    exit:
+    mov ah,4ch
     int 21h
-
-main endp
-end main
+    main endp
+end mai
